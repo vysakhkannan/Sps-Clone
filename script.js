4fe2b39dc722c60e230fb5b1984c275451e768cd
vysakhkannan <vysakhkannan811@gmail.com>
2021-09-26 19:03:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+function openVideo(image,video){
+
+    document.getElementById(image).style.display="none"
+    document.getElementById(video).style.display="block"
+
+    var video1= document.getElementById(video)
+    video1.src+="?autoplay=1"
+    
+    
+
+
+}
+// function openVideo2(){
+//     document.getElementById("image1").style.display="none"
+//     var video2=document.getElementById("vide1")
+//     video2.style.display="block"
+//     // video2.src+="autoplay=1"
+
+
+// }
+// function openVideo3(){
+//     document.getElementById("image2").style.display="none"
+//     var video3=document.getElementById("vide2")
+//     video3.style.display="block"
+//     // video3.src+="autoplay=1"
+
+// }
+
+// function openVideo4(){
+//     document.getElementById("image3").style.display="none"
+//     var video4=document.getElementById("vide3")
+//     video4.style.display="block"
+//     // video4.src+="autoplay=1"
+
+
+// }
+// function openVideo5(){
+//     document.getElementById("image4").style.display="none"
+//     var video5=document.getElementById("vide4")
+//     video5.style.display="block"
+//     // video5.src+="autoplay=1"
+
+
+// }
